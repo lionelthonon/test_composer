d609f2939eaabe22a5805d9e8d042e0a1622ea35
Lionel Thonon <lionel.thonon@gmail.com>
2015-03-12 09:47:03 +0100

    Adding a second class to the 'Test' namespace

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 /* Crée un alias
  * Uniquement dans les fichiers -- N'est pas traité par PHP
  */
-use Test/MaClasse as MaSuperClasse
+//use Test\MaClasse as MaSuperClasse;
+//use Test\MonAutreClasse as UneDeuxiemeClasse;
 
 /* Inclus la classe */
 //include('classes/MaClasse.php');
-    require 'vendore/autoload.php';
+/* Grace aux namespace, toutes les classes sont 'includes' en une seule ligne */
+    require 'vendor/autoload.php';
 
-echo get_class(new MaSuperClasse());
+echo get_class(new Test\MaClasse()).'<br />';
+echo get_class(new Test\MonAutreClasse());
